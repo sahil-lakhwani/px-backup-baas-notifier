41abdc19545c59e3601613822fbce08b568a4adb
Sahil Lakhwani <sahilakhwani@gmail.com>
2022-09-29 10:12:03 +0530

    check scheduler status each time (#8)
    Signed-off-by: Sahil Lakhwani <sahilakhwani@gmail.com>

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,8 +3,9 @@ package types
 type Status string
 
 const (
-	NOTFOUND  Status = "NotFound"
-	AVAILABLE Status = "Available"
-	PENDING   Status = "Pending"
-	FAILED    Status = "Failed"
+	NOTFOUND     Status = "NotFound"
+	AVAILABLE    Status = "Available"
+	PENDING      Status = "Pending"
+	FAILED       Status = "Failed"
+	Provisioning Status = "Provisioning"
 )
